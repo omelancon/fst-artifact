c44c7ae0f66e0ce99b8e2d9439da96b44ac872ee
omelancon <ol.melancon@gmail.com>
2025-07-15 12:55:13 +0200

    revert bigloo vs alloc time to 1-tag in plot.sh

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -212,7 +212,7 @@ cat >> $legendfile <<EOF
 \newcommand{\legendbiglootimealloc}{
 \begin{center}
 \begin{tabular}{l}
-\colorrect{self-tagging (3-tag)}{$COLORFLT}
+\colorrect{self-tagging (1-tag)}{$COLORFLTONE}
 \end{tabular}
 \end{center}
 }
@@ -229,7 +229,7 @@ cat >> $legendfile <<EOF
 EOF
 
 plot $PLOTDIR/gambit_time_alloc_$host.pdf "#$COLORFLTFOUR" "7,2" "3" "off" "0.2" "" "[0.2:2.9]" $STATS/gambit_0.stat $STATS/gambit_4.stat
-plot $PLOTDIR/bigloo_time_alloc_$host.pdf "#$COLORFLT" "7,2" "3" "off" "0.2" "" "[0.2:2.9]" $STATS/bigloo.stat $STATS/bigloo_flt.stat
+plot $PLOTDIR/bigloo_time_alloc_$host.pdf "#$COLORFLTONE" "7,2" "3" "off" "0.2" "" "[0.2:2.9]" $STATS/bigloo.stat $STATS/bigloo_flt1.stat
 
 #*---------------------------------------------------------------------*/
 #*    COMP_time_mantissa_ARCH.pdf                                      */
