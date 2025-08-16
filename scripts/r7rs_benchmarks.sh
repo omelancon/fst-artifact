c1f04457cb3b1d7ade5197c2a0c530e97aad1fa2
omelancon <ol.melancon@gmail.com>
2025-08-16 10:59:07 +0200

    revert R7RS benchmark Chez files to old version that included a R7RS wrapper

--- scripts/r7rs_benchmarks.sh
+++ scripts/r7rs_benchmarks.sh
@@ -8,4 +8,10 @@ color=39
 # download
 if [ "$action " = "download " -o "$action " = "all " ]; then
   download_git || exit 1
+  (cd $downloaddir && \
+   git restore --source=882b28c9fe931a912f7d670816e5d14454e246a5 -- \
+     src/Chez-prelude.scm \
+     src/Chez-postlude.scm && \
+   git show 7357ac5b2b59c88a6f2e5e7267d2da8259a6ce92 -- bench | git apply -R --3way --index)
+   (cd $downloaddir && git checkout --theirs bench)
 fi
