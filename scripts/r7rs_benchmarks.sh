bac8c1fc706340e8051165e427d9dd1ce54d81f0
omelancon <ol.melancon@gmail.com>
2025-08-16 11:07:58 +0200

    better log of R7RS benchmark installation

--- scripts/r7rs_benchmarks.sh
+++ scripts/r7rs_benchmarks.sh
@@ -12,6 +12,6 @@ if [ "$action " = "download " -o "$action " = "all " ]; then
    git restore --source=882b28c9fe931a912f7d670816e5d14454e246a5 -- \
      src/Chez-prelude.scm \
      src/Chez-postlude.scm && \
-   git show 7357ac5b2b59c88a6f2e5e7267d2da8259a6ce92 -- bench | git apply -R --3way --index)
-   (cd $downloaddir && git checkout --theirs bench)
+   git show 7357ac5b2b59c88a6f2e5e7267d2da8259a6ce92 -- bench | git apply -R --3way --index) >> $log 2>&1
+   (cd $downloaddir && git checkout --theirs bench) >> $log 2>&1
 fi
